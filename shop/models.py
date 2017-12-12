c541d35a1858e9e569f912f343ba58e3c7ff6b1b
David Mwangi <njorogedavid13@gmail.com>
2017-12-12 12:08:41 +0300

    make profile class and methods to save, get all and , get specific shop
    make test cases to test instance and save profile

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ShopProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    latitude=models.FloatField()
+    longitude=models.FloatField()
+    imageprofile=models.ImageField(upload_to='profile/')
+    shopname=models.CharField(max_length=50)
+
+    def get_all_shops(self):
+        all_shops=ShopProfile.objects.all()
+        return all_shops
+
+    @classmethod
+    def get_shop(cls,shop_id):
+        shop=ShopProfile.objects.filter(user=shop_id)
+        return shop
+
+    @classmethod
+    def create_profile(cls,user,latitude,longitude,imageprofile,shopname):
+        new_profile=ShopProfile(user=user,latitude=latitude,longitude=longitude,imageprofile=imageprofile,shopname=shopname)
+        new_profile.save()
+        return True
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import ShopProfile
 
 # Create your tests here.
+
+class ShopProfileTextClass(TestCase):
+    def setUp(self):
+        self.david=User(username='david',password='sdfsdfsd')
+        self.david.save()
+        self.new_profile=ShopProfile(user=self.david,latitude=12.0,longitude=3.6,imageprofile='sdfs',shopname='sdfdsfsd')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,ShopProfile))
+        
